27d59886ed4ddcb6bf3f4adf569b9a23c4ce1316
dkorneva <dariakorneva20@gmail.com>
2026-09-07 19:38:57 +0300

    projects section added

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -9,6 +9,7 @@ import ColorPicker from "../components/ColorPicker";
 import ProfessionalSummaryForm from "../components/ProfessionalSummaryForm";
 import ExperienceForm from "../components/ExperienceForm";
 import EducationForm from "../components/EducationForm";
+import ProjectsForm from "../components/ProjectForm";
 
 const ResumeBuilder = () => {
 
@@ -133,6 +134,10 @@ const ResumeBuilder = () => {
 
                 {activeSection.id === "education" && (
                   <EducationForm data={resumeData.education} onChange={(data) => setResumeData(prev => ({...prev, education: data}))}/>
+                )}
+
+                {activeSection.id === "projects" && (
+                  <ProjectsForm data={resumeData.project} onChange={(data) => setResumeData(prev => ({...prev, project: data}))}/>
                 )}
               </div>
 						</div>
