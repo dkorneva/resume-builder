29fa0e49e311832742dc8dc628912515fe0d7115
dkorneva <dariakorneva20@gmail.com>
2026-09-07 20:05:44 +0300

    skills section added

--- client/src/pages/ResumeBuilder.jsx
+++ client/src/pages/ResumeBuilder.jsx
@@ -10,6 +10,7 @@ import ProfessionalSummaryForm from "../components/ProfessionalSummaryForm";
 import ExperienceForm from "../components/ExperienceForm";
 import EducationForm from "../components/EducationForm";
 import ProjectsForm from "../components/ProjectForm";
+import SkillsForm from "../components/SkillsForm";
 
 const ResumeBuilder = () => {
 
@@ -138,6 +139,10 @@ const ResumeBuilder = () => {
 
                 {activeSection.id === "projects" && (
                   <ProjectsForm data={resumeData.project} onChange={(data) => setResumeData(prev => ({...prev, project: data}))}/>
+                )}
+
+                {activeSection.id === "skills" && (
+                  <SkillsForm data={resumeData.skills} onChange={(data) => setResumeData(prev => ({...prev, skills: data}))}/>
                 )}
               </div>
 						</div>
